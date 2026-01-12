13820fb17e37013dfd5166ca2ee22d542c877ce3
Julio Osores <julio.osores61@gmail.com>
2026-01-11 22:24:55 -0500

    Fix indentation for render_header function

--- app.py
+++ app.py
@@ -214,7 +214,8 @@ responsables = sorted([r for r in df_ue["Responsable_Institucional"].unique() if
 def get_image_base64(path):
     with open(path, "rb") as f:
         return base64.b64encode(f.read()).decode()
-        def render_header():
+
+def render_header():
     logo_base64 = get_image_base64("logo.png")
 
     st.markdown(
